4d7712e2469ef6fea5cbdc29e765afd0347282ab
조수빈 <angelajsb@naver.com>
2023-12-27 11:58:03 +0900

    12/27 db prac

--- mariadbwork/13_constraints.sql
+++ mariadbwork/13_constraints.sql
@@ -22,12 +22,12 @@ VALUES
 
 SELECT * FROM user_notnull;
 
-
+-- user_id NULL error
 INSERT
   INTO user_notnull
 (user_no, user_id, user_pwd, user_name, gender, phone, email)
 VALUES
-(3, 'user03', '이순신', '남', '010-222-2222', 'lee222@gmail.com'); 
+(3, 'user03', NULL, '남', '010-222-2222', 'lee222@gmail.com'); 
 
 -- 2. UNIQUE
 
@@ -53,6 +53,7 @@ VALUES
 
 SELECT * FROM user_unique;
 
+-- PHONE duplicate error
 INSERT 
   INTO user_unique
 (user_no, user_id, user_pwd, user_name, gender, phone, email)
@@ -84,22 +85,65 @@ VALUES
 
 SELECT * FROM user_primarykey;
 
--- PRIMARY KET NULL값 불가능
+-- PRIMARY KET NULL error
 INSERT 
   INTO user_primarykey
 (user_no, user_id, user_pwd, user_name, gender, phone, email)
 VALUES
 (null, 'user03', 'pass03', '이순신', '남', '010-777-7777', 'lee222@gmail.com');
 
--- PRIMARY KEY값 중복 불가능
+-- PRIMARY KEY duplicate error
 INSERT 
   INTO user_primarykey
 (user_no, user_id, user_pwd, user_name, gender, phone, email)
 VALUES
 (2, 'user03', 'pass03', '이순신', '남', '010-777-7777', 'lee222@gmail.com');
 
+-- -----------------------------------
+-- 4. FORIEGN KEY 
+-- * 1:多 관계 = 부모:자식 EX) 부서(부모):사원(자식)
 
 
+-- 4-1. 부모 테이블(회원등급) 선 생성 
+DROP TABLE IF EXISTS user_grade;
+
+CREATE TABLE IF NOT EXISTS user_grade (
+    grade_code INT NOT NULL UNIQUE,
+    grade_name VARCHAR(255) NOT NULL
+) ENGINE=INNODB;
+
+INSERT 
+  INTO user_grade
+VALUES 
+(10, '일반회원'), -- 여러 데이터들이 10, 20, 30 중에 포함될 것이기 때문에 부모테이블 (먼저 생성)
+(20, '우수회원'),
+(30, '특별회원');
+
+SELECT * FROM user_grade;
+
+-- 4-2. 자식 테이블(회원) 후 생성
+
+DROP TABLE IF EXISTS user_foreignkey1;
+CREATE TABLE IF NOT EXISTS user_foreignkey1 (
+    user_no INT PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    user_pwd VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    gender VARCHAR(3),
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    grade_code INT ,
+    FOREIGN KEY (grade_code) REFERENCES user_grade (grade_code) -- 부모 코드를 참조할 것이다.
+) ENGINE=INNODB;
+
+INSERT 
+  INTO user_foreignkey1
+(user_no, user_id, user_pwd, user_name, gender, phone, email, grade_code)
+VALUES
+(1, 'user01', 'pass01', '홍길동', '남', '010-1234-5678', 'hong123@gmail.com', 10),
+(2, 'user02', 'pass02', '유관순', '여', '010-777-7777', 'yu77@gmail.com', 20);
+
+SELECT * FROM user_foreignkey1;
 
 
 
