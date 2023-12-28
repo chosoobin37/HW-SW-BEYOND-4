eaedc417bcd51c8333a1c9df8b561cd3a492dd2e
조수빈 <angelajsb@naver.com>
2023-12-28 18:07:34 +0900

    12/28 db prac

--- mariadbwork/18_trigger.sql
+++ mariadbwork/18_trigger.sql
@@ -15,6 +15,8 @@ END//
 
 DELIMITER ;
 
+DROP TRIGGERS after_order_menu_insert;
+SHOW TRIGGERS;
 -- 주문 테이블(TBL_ORDER)에 INSERT 후 주문메뉴테이블(TBL_ORDER_MENU)에
 -- 주문한 메뉴마다 INSERT 후 주문테이블에 총금액 업데이트 확인
 
@@ -84,6 +86,121 @@ DELETE FROM tbl_order_menu WHERE 1 = 1;
 
 -- 이후 AUTO_INCREMENT도 다시 초기화
 ALTER TABLE tbl_order AUTO_INCREMENT = 1;
+
+-- ------------------------------------------------------------
+-- 상품 입출과 관련 trigger 활용 예제
+
+DROP TABLE pro_detail;
+DROP TABLE product;
+
+CREATE TABLE PRODUCT(           -- 이력 테이블(UPDATE가 발생하는 테이블)
+  PCODE INT PRIMARY KEY auto_increment,
+  PNAME VARCHAR(30),
+  BRAND VARCHAR(30),
+  PRICE INT,
+  STOCK INT DEFAULT 0,
+  CHECK(STOCK >= 0)
+);
+
+CREATE TABLE PRO_DETAIL(        -- 내역 테이블(INSERT가 발생하는 테이블)
+  DCODE INT PRIMARY KEY auto_increment,
+  PCODE INT,
+  PDATE DATE,
+  AMOUNT INT,
+  STATUS VARCHAR(10) CHECK(STATUS IN ('입고', '출고')),
+  FOREIGN KEY(PCODE) REFERENCES PRODUCT  -- 어차피 해당 테이블의 PK컬럼에 대한 외래키이므로 PK컬럼은 적지 않아도 된다.(알아서 인식함)
+);
+
+SELECT * FROM product;
+
+INSERT
+  INTO PRODUCT
+(
+  PCODE, PNAME, BRAND
+, PRICE, STOCK
+)
+VALUES
+(
+  null, '갤럭시플립', '삼송'
+, 900000, 5
+);
+
+INSERT
+  INTO PRODUCT
+(
+  PCODE, PNAME, BRAND
+, PRICE, STOCK
+)
+VALUES
+(
+  null, '아이펀 14', '애펄'
+, 1000000, 5
+);
+
+INSERT
+  INTO PRODUCT
+(
+  PCODE, PNAME, BRAND
+, PRICE, STOCK
+)
+VALUES
+(
+  null, '투명폰 3', '헬쥐'
+, 1000000, 5
+);
+
+SELECT * FROM PRODUCT;
+SELECT * FROM PRO_DETAIL;
+
+INSERT
+  INTO PRO_DETAIL
+(
+  DCODE, PCODE, PDATE
+, AMOUNT, STATUS
+)
+VALUES
+(
+  null, 1, CURDATE()
+, 5, '입고'
+);
+
+INSERT
+  INTO PRO_DETAIL
+(
+  DCODE, PCODE, PDATE
+, AMOUNT, STATUS
+)
+VALUES
+(
+  null, 2, CURDATE()
+, 2, '출고'
+);
+
+SELECT * FROM PRODUCT;
+SELECT * FROM PRO_DETAIL;
+
+-- -------------------------------------------------------------
+DELIMITER //
+CREATE OR REPLACE TRIGGER TRG_PRODUCTAFTER 
+AFTER INSERT 
+ON PRO_DETAIL
+FOR EACH ROW
+BEGIN
+  IF NEW.STATUS = '입고' THEN
+    UPDATE PRODUCT A
+       SET A.STOCK = A.STOCK + NEW.AMOUNT
+     WHERE A.PCODE = NEW.PCODE;
+  ELSEIF NEW.STATUS = '출고' THEN
+    UPDATE PRODUCT A 
+       SET A.STOCK = A.STOCK - NEW.AMOUNT
+     WHERE A.PCODE = NEW.PCODE;
+  END IF;
+END;
+//
+DELIMITER ;
+-- -------------------------------------------------------------
+
+SHOW TRIGGERS;
 
 
  	
