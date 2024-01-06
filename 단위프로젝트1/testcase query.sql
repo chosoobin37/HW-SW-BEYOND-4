6b323449e544be8e366bdb25778167a336a47968
조수빈 <angelajsb@naver.com>
2024-01-06 16:16:13 +0900

    testcase query upadate

--- 단위프로젝트1/testcase query.sql
+++ 단위프로젝트1/testcase query.sql
@@ -25,11 +25,23 @@ SELECT * FROM AnimalCategory;
 SELECT * FROM Animal_type_category;
 
 INSERT INTO Animal_type_category VALUES 
-(4,1)
+ (1,1)
+,(1,2)
+,(1,3)
+,(2,1)
+,(2,2)
+,(2,3)
+,(3,1)
+,(3,2)
+,(3,3)
+,(4,1)
 ,(4,2)
-,(4,3);
- 
-s
+,(4,3)
+,(5,1)
+,(5,2)
+,(5,3);
+
+
  
  
  
